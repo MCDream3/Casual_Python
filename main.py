65b4fa3ba5ae90b71f60f0af4da440b3d3c90ee1
27tr <114559221+MCDream3@users.noreply.github.com>
2022-10-12 06:29:20 +0000

    Karta pracy 3 skończona main.py

---
+++ main.py
@@ -0,0 +1,89 @@
+# p, q = int(input()), int(input())
+# if (p + 0.3*p > q):
+#     print("tak")
+# else:
+#     print("nie")
+
+
+# a = 1
+# for a in range(10,22):
+#     print (a)
+
+# for i in range(15,32,2): print(i, end=" ")
+
+# for i in range(99,9,-1): print(i, end=" ")
+
+# for i in range(10,100,1): print(109-i, end=" ")
+
+# suma = 0
+# for i in range(10, 100):
+#      suma = suma + i
+# print(suma)
+
+# n = int(input())
+# suma = n*(n+1) // 2
+# for i in range(n-1):
+#     x = int(input())
+#     suma = suma - x
+# print(suma)
+
+# pętla liczb dwucyfrowych od 10 do 21
+# for i in range(10,22):
+#   print(i)
+# # pętla licz dwucyfrowych nieparzystych od 15 do 31
+# for i in range(15,32,2):
+#   print (i)
+# # pętla liczb dwucyfrowych malejąco (step ujemny)
+# for i in range(99,9,-1):
+#   print(i, end=" ")
+# # pętla liczb dwucyfrowych malejąco (step dodatni)
+# for i in range(10,100,1):
+#   print(109-i, end=" ")
+# pętla liczb trycyfrowych podzielnych przez 20
+# for i in range(100,1000,20):
+#   print(i, end=" ")
+# pętla liczb trycyfrowych podzielnych przez 20
+# for i in range(5, 50, 1):
+#   print(i*20, end=" ")
+
+#Zad 1
+# n = int(input())
+# for i in range(n):
+#   print(i**3-3, end=" ")
+
+# #Zad 2
+# for i in range(105,1000,15):
+#   print(i, end=" ")
+
+# #Zad 3
+# n = int(input())
+# for i in range(1,n+1):
+#   if n % i == 0:
+#     print(i, end=" ")
+
+# #Zad 4
+# suma = 0
+# for i in range(10,100):
+#   suma = suma + i
+# print(suma)
+  
+#Zad 5
+# n = int(input())
+# suma = n*(n+1)//2
+
+# for i in range(n-1):
+#   x = int(input())
+#   suma = suma - x
+# print("Nie podałeś:",suma)
+
+#Zad 6
+# n = int(input())
+# a = 0
+# b = 1
+# print(a, end=" ")
+# print(b, end=" ")
+# for i in range(n-1):
+#   a, b = b, a+b
+#   print(b, end=" ")
+
+  

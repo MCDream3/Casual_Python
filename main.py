1114804cb681b9d19ba81510ef7f41e0c47c8957
27tr <114559221+MCDream3@users.noreply.github.com>
2022-11-01 18:09:27 +0000

    skończona karta pracy 3b

---
+++ main.py
@@ -0,0 +1,56 @@
+#Karta pracy 3b
+#Zad 1
+# for i in range(1,31):
+#   print(i, end=" ")
+# #Zad 2
+# for i in range(1,100,2):
+#   print(i**2, end=" ")
+# #Zad 3
+# for i in range(1000,10001):
+#   if i % 379 == 0:
+#     print(i)
+#     #Zad 4
+# for i in range(100,1000):
+#   if i%11==0:
+#     print(i)
+#   #Zad 5
+# suma=0
+# ile = int(input("ile jest liczb:"))
+# n = int(input("podaj liczbe:"))
+# suma=suma+n
+# for i in range(0,ile):
+#   if i==ile-1:
+#     print(suma)
+# # #Zad 6
+# k=int(input("ile liczb poczatkowych?:"))
+# suma=0
+# for i in range(0,k*2,2):
+#   suma=suma+i
+# if i==k*2-2:
+#   print(suma)
+# #Zad 7
+# n=int(input("ile liczb poczatkowych?:"))
+# suma=0
+# for i in range(11,10+((n*2)+1),2):
+#   suma=suma+i
+#   if i==10+n*2-1:
+#     print(suma)
+# #Zad 8
+# n=int(input("kwota poczatkowa:"))
+# lata=int(input("ile lat?:"))
+# miesiace=lata/12
+# for i in range(lata,lata+1):
+#   print(n+n*((6*i)/100))
+# #Zad 9
+# n=int(input("liczba:"))
+# n=n*100
+# for i in range(21,n,100):
+#   print(i)
+# #Zad 10 :D
+# import math
+# a=0
+# for i in range(1,1000):
+#   if i%10==0:
+# a=i
+# elif i-a==mathsqrt(i):
+#   print(i)
